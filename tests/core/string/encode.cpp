6bbdcd618b6b8c1ff1076d2dbb47b108c5483ec2
Matt Love <matt.love@360dialog.com>
2017-01-31 18:43:03 +0100

    Add simpler overloads for hex_encode and hex_decode

---
+++ tests/core/string/encode.cpp
@@ -0,0 +1,24 @@
+/*
+ * Part of commonpp.
+ *
+ * Distributed under the 2-clause BSD licence (See LICENCE.TXT file at the
+ * project root).
+ *
+ * Copyright (c) 2017 Thomas Sanchez.  All rights reserved.
+ *
+ */
+
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+#include <commonpp/core/string/encode.hpp>
+
+using namespace commonpp::string;
+
+BOOST_AUTO_TEST_CASE(hex_encode_decode)
+{
+    std::string test("deafbeef");
+   
+    BOOST_CHECK_EQUAL(test, hex_encode(hex_decode(test)));
+}
+
